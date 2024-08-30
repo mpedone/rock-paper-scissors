5072cd8d25de62b80a2c957abf3cb3ef337325d9
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-08-30 17:47:17 -0400

    Create function to prompt user for a choice
    Used a while loop to keep asking until the user types in one of the
    three options or cancels. Not sure what will happen to the game if the
    user cancels, this might not have been needed, but the instructions
    aren't super clear. I'm thinking I could return 'null' or some other
    value that would cause the computer to win, or just cause the game to
    end.

--- rps.js
+++ rps.js
@@ -1,5 +1,22 @@
 function getComputerChoice() {
-    choices = ["rock", "paper", "scissors"];
-    choice = choices[Math.floor(Math.random()*3)];
+    // set the computer's choice
+    let choices = ["rock", "paper", "scissors"];
+    let choice = choices[Math.floor(Math.random()*3)];
     return choice;
+}
+
+function getHumanChoice() {
+    // prompt for human choice
+    humanChoices = ["rock", "paper", "scissors"];
+    
+    let keepAsking = 1;
+    while(keepAsking) {
+        let humanChoice = prompt("Please pick a weapon.");
+        if(humanChoice == null){
+            keepAsking = 0;
+        } else if (humanChoices.includes(humanChoice.toLowerCase())) {
+            return humanChoice.toLowerCase();
+            keepAsking = 0;
+        }
+    }    
 }
