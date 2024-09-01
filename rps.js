732b81a0b72c79313e85f8dd2d72f0f8fb40f77f
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-09-01 15:21:55 -0400

    Create a function to play one round of the game
    Used nested control flow - if/else inside a switch inside an if/else. I
    feel like there is a smoother way to do this, but it's something I'll
    hold of on trying to do until later. Could be a good exercise. Had an
    issue with the if/else statements in the switch, as I was assigning
    not comparing. Used the debugger to suss out that issue, which, while
    it feels like a rookie mistake, I'm proud I was able to step through
    and see what my code was actually doing.

--- rps.js
+++ rps.js
@@ -24,3 +24,51 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    // Compare selections
+    let result = ""
+    if (humanChoice === computerChoice) {
+        result = `It's a tie!`
+        humanScore += 1;
+        computerScore += 1;
+    } else {
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "scissors") {
+                    result = 'Rock smashes scissors, user wins!';
+                    humanScore += 1;
+                } else if (computerChoice === "paper") {
+                    result = 'Paper covers rock, computer wins!';
+                    computerScore += 1;
+                }
+                break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    result = 'Paper covers rock, user wins!';
+                    humanScore += 1;
+                } else if (computerChoice === "scissors") {
+                    result = 'Scissors cut paper, computer wins!';
+                    computerScore += 1;
+                }
+                break;
+            case "scissors":
+                if (computerChoice === "paper") {
+                    result = 'Scissors cut paper, user wins!';
+                    humanScore += 1;
+                } else if (computerChoice === "rock") {
+                    result = 'Rock smashes scissors, computer wins!';
+                    computerScore += 1;
+                }
+                break;
+        }
+    }
+    // increment appropriate score counter
+    console.log(`User selected ${humanChoice}, computer selected ${computerChoice}... ${result}`);
+    console.log(`User: ${humanScore}, Computer: ${computerScore}`)
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+
+playRound(humanSelection, computerSelection);
